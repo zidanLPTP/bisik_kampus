93acca02dae81e9c2efaff8660e989fa44257501
zidanLPTP <zidanlptp@gmail.com>
2025-11-22 13:26:01 +0700

    update

--- api/index.php
+++ api/index.php
@@ -2,3 +2,8 @@
 
 // api/index.php
 require __DIR__ . '/../public/index.php';
+
+$app->useStoragePath('/tmp/storage');
+
+// ... lanjut ke kode bawahnya
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
